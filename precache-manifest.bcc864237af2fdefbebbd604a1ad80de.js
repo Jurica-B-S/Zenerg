5377a15011344f05c0a65fa3757179d6d71ec843
Ubuntu <ubuntu@ip-172-26-7-218.eu-central-1.compute.internal>
2022-09-25 11:31:24 +0000

    Second commit

---
+++ precache-manifest.bcc864237af2fdefbebbd604a1ad80de.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f6b6e69473f10526ce3b39a43f6fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4601f5f4ad5a566fbf3",
+    "url": "/static/css/2.9bc51dba.chunk.css"
+  },
+  {
+    "revision": "ff3215db07cc77b6961b",
+    "url": "/static/css/main.b7c1a810.chunk.css"
+  },
+  {
+    "revision": "d4601f5f4ad5a566fbf3",
+    "url": "/static/js/2.4a3b8db5.chunk.js"
+  },
+  {
+    "revision": "e78294685cd6bd1fcf5fedba7fc9fa0d",
+    "url": "/static/js/2.4a3b8db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24000554cfe04c5a4d91",
+    "url": "/static/js/3.068e7dac.chunk.js"
+  },
+  {
+    "revision": "ff3215db07cc77b6961b",
+    "url": "/static/js/main.e56cca35.chunk.js"
+  },
+  {
+    "revision": "6e06a1e4732b64c9787b",
+    "url": "/static/js/runtime~main.242f5c43.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
